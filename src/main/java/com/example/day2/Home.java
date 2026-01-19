e9b02a2806c54362501fb81fdb1a9ba35f442dbc
Ankur Mittal <ankurmittal9081@gmail.com>
2026-01-19 12:53:25 +0530

    commit

--- src/main/java/com/example/day2/Home.java
+++ src/main/java/com/example/day2/Home.java
@@ -4,9 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Home {
+public class Home{
+
     @GetMapping("/")
-    public String hello(){
-        return  "index";
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "Home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "About";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "Contact";
     }
 }
+
